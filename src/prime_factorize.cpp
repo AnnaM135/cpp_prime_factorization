6185fe468f44a1d34cef168de1475bb345a6c8f5
AnnaM135 <anna.melqonian@gmail.com>
2025-03-22 17:50:01 +0400

    with TDD generationg prodoction code

--- src/prime_factorize.cpp
+++ src/prime_factorize.cpp
@@ -3,7 +3,22 @@
 std::vector<int> primeFactorize(int n)
 {
     std::vector<int> factors;
-    if(n == 1)
-        factors.push_back(1);
+    int divisor = 2;
+    if(n <=1) 
+        return std::vector<int>{};
+    while(divisor < n)
+    {
+        while(n % divisor == 0)
+        {
+            factors.push_back(divisor);   
+            n = n / divisor;
+        }
+        divisor++;
+    }
+
+    if(n > 1)
+    {
+        factors.push_back(n);
+    }
     return factors;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -2,16 +2,65 @@
 #include <gtest/gtest.h>
 #include "../src/prime_factorize.h" 
 
-// Test case for 0
+// Test case for 0 and 1
 TEST(PrimeFactorizeTest, HandlesZero)
 {
     std::vector<int> empty;
     EXPECT_EQ(primeFactorize(0), empty);
+    EXPECT_EQ(primeFactorize(1), empty);
 }
 
-// Test case for 1
-TEST(PrimeFactorizeTest, HandlesOne)
+// Test case for PrimeNumbers
+TEST(PrimeFactorizeTest, PrimeNumbers)
 {
-    std::vector<int> one = {1};
-    EXPECT_EQ(primeFactorize(1), one);
+    // std::vector<int> prime = {2};
+    EXPECT_EQ(primeFactorize(2), std::vector<int>{2});
+    EXPECT_EQ(primeFactorize(3), std::vector<int>{3});
+    EXPECT_EQ(primeFactorize(5), std::vector<int>{5});
+    EXPECT_EQ(primeFactorize(7), std::vector<int>{7});
+}
+
+TEST(PrimeFactorizeTest, NonPrime4)
+{
+    std::vector<int> non_prime = {2, 2};
+    EXPECT_EQ(primeFactorize(4), non_prime);
+}
+
+TEST(PrimeFactorizeTest, NonPrime6)
+{
+    std::vector<int> non_prime = {2, 3};
+    EXPECT_EQ(primeFactorize(6), non_prime);
+}
+
+TEST(PrimeFactorizeTest, NonPrime8)
+{
+    std::vector<int> non_prime = {2, 2, 2};
+    EXPECT_EQ(primeFactorize(8), non_prime);
+}
+
+TEST(PrimeFactorizeTest, NonPrime9)
+{
+    std::vector<int> non_prime = {3, 3};
+    EXPECT_EQ(primeFactorize(9), non_prime);
+}
+
+TEST(PrimeFactorizeTest, NonPrime10)
+{
+    std::vector<int> non_prime = {2, 5};
+    EXPECT_EQ(primeFactorize(10), non_prime);
+}
+
+TEST(PrimeFactorize, LargeNumber12) {
+    std::vector<int> large_number = {2, 2, 3};
+    EXPECT_EQ(primeFactorize(12), large_number);  
+}
+
+TEST(PrimeFactorize, LargeNumber60) {
+    std::vector<int> large_number = {2, 2, 3, 5};
+    EXPECT_EQ(primeFactorize(60), large_number); 
+}
+
+TEST(PrimeFactorize, LargeNumber125) {
+    std::vector<int> large_number = {5, 5, 5};
+    EXPECT_EQ(primeFactorize(125), large_number);
 }
